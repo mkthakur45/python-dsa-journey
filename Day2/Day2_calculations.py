11dc5a4fdf662b90f181ec5c24a1fcf200095ef4
manishcodes <whykishu@gmail.com>
2026-02-01 21:29:03 +0530

    Add Day 2 calculations

---
+++ Day2/Day2_calculations.py
@@ -0,0 +1,12 @@
+# Day 2 - Simple Calculation
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+diff = a - b
+mul = a * b
+
+print("Sum =", sum)
+print("Difference =", diff)
+print("Multiplication =", mul)
