6f9b18200ddcbd6e62fccc4f2458f49390ccc032
manishcodes <whykishu@gmail.com>
2026-02-04 21:28:02 +0530

    percentage

---
+++ percentage.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter marks of subject 1: "))
+m2 = int(input("Enter marks of subject 2: "))
+m3 = int(input("Enter marks of subject 3: "))
+
+total = m1 + m2 + m3
+percentage = (total / 300) * 100
+
+print("Total Marks =", total)
+print("Percentage =", percentage)
